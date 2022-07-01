d50919ebfad04d597a85db51778c80b14b978078
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-01 21:49:47 +0000

    Compare number to know it's sign
    Positive anything is better than negative nothing!
    If the number is 0: is zero.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: This program will assign a random number to the
+* variable n each time it is executed.
+* Return: Always (0)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d, %s\n", n, n > 0 ? "is positive" : n == 0 ? "is zero" : "is negative");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+# C - Variables, if, else, while
+This directory is for tasks on variables and control flow statements.
