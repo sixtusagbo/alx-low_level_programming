689024493bebb68bae59e0c710be6910c18b7e9e
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-08 05:16:24 +0100

    Fix a function

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,6 +11,7 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	if (a > b && a > c)
 	{
 		largest = a;
@@ -19,9 +20,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b)
 	{
 		largest = c;
+	}
+	else
+	{
+		largest = b;
 	}
 
 	return (largest);
