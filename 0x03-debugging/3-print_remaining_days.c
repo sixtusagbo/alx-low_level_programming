7ad748943c8a55e7485db4bab97dd63973e2620b
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-08 18:08:54 +0100

    Fix the remaining days file in 0x03 last task

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,22 +2,22 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how many
- * day areprint_remaining_days - takes a date and prints how many day are
+ * print_remaining_days - takes a date and prints how many day are
  * left in the year, taking leap year into account
- * @month: month in number format
- * @day: day of month
- * @year: year
+ * @month: month in digit
+ * @day: day of month in digit
+ * @year: year in digit
  * Return: void
  */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 && !(year % 100 == 0)) || year % 400 == 0)
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
