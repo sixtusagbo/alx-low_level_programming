0651c39c667393503f36e38f90a495756de5dfee
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-09 05:18:52 +0100

    Print most numbers
    The numbers speak for themselves.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers between 0 to 9 except 2 and 4.
+ * Return: nothing.
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 || i != 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
