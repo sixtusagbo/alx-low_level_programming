be781496ec7cde8b5f90db4576e259c943d0ba6a
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-09 05:29:05 +0100

    Print 10 times the numbers, from 0 to 14
    Numbers constitute the only universal language. I also fixed a bug in
    the previous task.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers between 0 to 9 except 2 and 4.
+ * print_most_numbers - prints the numbers between 0 to 9 except 2 and 4.
  * Return: nothing.
  */
 void print_most_numbers(void)
 {
-	int i;
+	char i;
 
 	for (i = 48; i < 58; i++)
 	{
-		if (i != 50 || i != 52)
+		if ((i != 50) && (i != 52))
 			_putchar(i);
 	}
 	_putchar('\n');
