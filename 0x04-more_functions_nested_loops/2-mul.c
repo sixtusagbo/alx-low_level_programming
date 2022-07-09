52329a06edeec5c1017102b6e1f2084e385d635d
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-09 05:08:13 +0100

    Multiply two integers
    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* mul - Multiplies two integers
+* @a: first integer
+* @b: second integer
+* Return: Multiple of a and b
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
