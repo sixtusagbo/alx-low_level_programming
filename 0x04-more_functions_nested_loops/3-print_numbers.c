4b36eb5c34250bfee91fa53953708f37f7d2cede
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-09 05:10:59 +0100

    Print numbers between 0 to 9
    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers between 0 to 9.
+ * Return: nothing.
+ */
+void print_numbers(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
