12cc946e94ac21a867d5dc91fe75eab5530e49a2
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-13 01:11:27 +0100

    Calculate string length
    This report, by its very length, defends itself against the risk of being read.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strlen - Calculate length of a string
+* @s: pointer to the string
+*
+* Return: number of characters in the string
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
