0b00e296075307427c3c4be9809f6a443ad080f7
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-13 01:40:36 +0100

    Print a string
    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - Prints a string followed by a new line
+* @str: String to be printed.
+*
+* Return: nothing.
+*/
+void _puts(char *str)
+{
+	puts(str);
+}
