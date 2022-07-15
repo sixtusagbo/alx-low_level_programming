d96e35fd7407b14b1126a57231a4974ee5032e9a
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-15 04:16:57 +0100

    Reverse content of integer array
    I am a kind of paranoid in reverse. I suspect people of plotting to
    make me happy

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "_putchar.c"
+
+/**
+* reverse_array - Reverse the content of an array of integers
+* @a: an array of integers
+* @n: number of elements of the array
+*
+* Return: nothing.
+*/
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
+}
