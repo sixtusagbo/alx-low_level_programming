cb6fd4fecfb5aca8cc0b454a3f9949bd23f7492f
sixtusmiracle <miracleagbosixtus@gmail.com>
2022-07-15 04:57:32 +0100

    Print an integer
    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,5 +11,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
 
 #endif
