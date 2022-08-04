5ab50f4b531913bae839355b554f2be8a5a97ed6
sixtusagbo <miracleagbosixtus@gmail.com>
2022-08-04 05:35:21 +0100

    Execute a function on each element of an array
    If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stdlib.h>
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
