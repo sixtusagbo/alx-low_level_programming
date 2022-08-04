4df94a93b7cfacbfc86971390cbd0271871c2b26
sixtusagbo <miracleagbosixtus@gmail.com>
2022-08-04 05:44:16 +0100

    Fixed function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,7 +9,7 @@
  *
  * Return: nothing
  */
-void array_iterator(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,7 +4,6 @@
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
-int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
