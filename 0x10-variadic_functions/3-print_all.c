a96019737a4130b205eb8c9e72e17217320b619b
sixtusagbo <miracleagbosixtus@gmail.com>
2022-08-05 03:14:00 +0100

    [Update] Print anything
    One woman's constant is another woman's variable
    To be is to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,61 @@
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: a list of types of arguments passed to the function
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	va_list arglist;
+	/* i and j - for looping, e - isEnd */
+	unsigned int i = 0, j, e = 0;
+	char *string;
+	/* supported types */
+	const char s_types[] = "cifs";
+
+	va_start(arglist, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (s_types[j])
+		{
+			if (format[i] == s_types[j] && e)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(arglist, int)), e = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(arglist, int)), e = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(arglist, double)), e = 1;
+			break;
+		case 's':
+			string = va_arg(arglist, char *), e = 1;
+
+			if (string)
+			{
+				printf("%s", string);
+				break;
+			}
+			else
+			{
+				printf("(nil)");
+				break;
+			}
+		} i++;
+	}
+
+	printf("\n");
+
+	va_end(valist);
+}
