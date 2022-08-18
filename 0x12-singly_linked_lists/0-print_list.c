0739f86e5477ca6bc788782799251ac91e191d0c
sixtusagbo <miracleagbosixtus@gmail.com>
2022-08-18 13:03:38 +0100

    [Feature] Print all elements of singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: singly linked list
+ *
+ * Return: number of elements in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t next_element;
+
+	next_element = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		next_element++;
+	}
+	return (next_element);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,5 +15,6 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+size_t print_list(const list_t *h);
 
 #endif /* LISTS_H */
